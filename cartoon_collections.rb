26bd10679f707c75da1406db6faaf00632ef5d8e
David Ricketts <david.ricketts119@gmail.com>
2020-01-18 23:19:07 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,39 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(names)
+  count = 1
+  names.collect do |name| 
+    puts "#{count}. #{name}"
+    count += 1 
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+
+
+def summon_captain_planet(elements)
+  augmented_elements =  elements.map {|element| element.capitalize + "!"}
+  return augmented_elements
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+
+
+
+def long_planeteer_calls(calls)
+  result = calls.find do |ele| 
+    if ele.length > 4 
+      return true 
+    end
+  end
+  if result == nil
+    return false 
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(ingreedient_list)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  result = nil
+  ingreedient_list.map do |ingreedient| 
+    if cheese_types.include? ingreedient
+      return ingreedient
+    end
+  end
+  return result
 end
