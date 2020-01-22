3d3e87dfaef7f69a5f1860120de6c38ea8dfc308
lior Shine <shinelior@gmail.com>
2020-01-22 00:26:07 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,64 @@
-def roll_call_dwarves# code an argument here
+require "pry"
+def roll_call_dwarves(s)# code an argument here
+
+#s=["Doc", "Dopey", "Bashful", "Grumpy"]
+new_array = []
+
+#y = 1
+s.each_with_index do |x,y| 
+  
+y += 1 
+puts "#{y}#{x}"
+end
+
   # Your code here
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planeteer_calls)
+  #planeteer_calls = ["carrot", "cucumber", "pepper"]
+  planeteer_calls.map do |x|  x.capitalize + "!"  end
+  
+  # code an argument here
   # Your code here
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(arra)
+  
+  y = []
+  i = 0
+ arra.any?{|w| w.length > 4}
+    
+ 
+   
+
+
+  # code an argument here
   # Your code here
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+
+def find_the_cheese(aar)
+ 
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+ 
+ aar.find do |snak| 
+  cheese_types.include?(snak) 
 end
+# binding.pry
+
+ 
+end
+
+
+ #chee = aar.find_all do |x|   x == "cheddar" || x == "gouda" || # x == "camembert" end
+ #chee = aar.find_all do |x| cheese_types end
+# else 
+ 
+  
+
+
+  
+     
+    
+      
