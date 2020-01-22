93d5b7ee5340edec0fe5d3bd9adf6770e942001c
Jack T <jackmt9@gmail.com>
2020-01-22 17:37:30 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,29 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require "pry"
+def roll_call_dwarves(array)
+  array.each_with_index do |dwarf, index|
+    print index + 1
+    puts " " + dwarf
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(calls)
+  calls.map {|call| call.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  longest_word = calls.map(&:length).max 
+  (longest_word > 4)? true:false
+  # binding.pry
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.each do |e|
+    if cheese_types.include?(e)
+      return e
+    end
+  end
+  return nil
+  # binding.pry
+  
 end
